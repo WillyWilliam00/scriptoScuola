3810d8811330253912076c1b5a0ec35c066a831a
William <133374335+WillyWilliam00@users.noreply.github.com>
2026-02-18 20:02:14 +0100

    frontend: Header, Footer e aggiornamento dipendenze (package.json)

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 bg-background border-t border-border py-2 px-4 ">
-      <div className="container mx-auto flex items-center justify-center text-sm text-muted-foreground">
+    <footer className="fixed bottom-0 left-0 right-0 bg-background border-t border-border py-2 px-4 h-12">
+      <div className="container mx-auto flex items-center justify-center text-sm text-muted-foreground h-full">
         <span>Copyright © {new Date().getFullYear()} William Costa</span>
         <span className="mx-2">•</span>
         <a 
